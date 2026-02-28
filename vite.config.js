c04e520ca287d9fe562eaddc7dec3313962feacd
Ganesh6548 <ganeshveerappan640@gmail.com>
2026-03-01 02:22:09 +0530

    Fix base/homepage to match repository name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Krystal-Aqua/',
+  base: '/Krystal_Aqua/',        // underscore to match repo name
   plugins: [react()],
 })
